55d70ee1ecba7901991a5471593f293510200282
Vivian <82780605+vivianknee@users.noreply.github.com>
2023-08-03 11:36:12 -0700

    Update BlenderStatusMonitor.py

--- BlenderStatusMonitor.py
+++ BlenderStatusMonitor.py
@@ -7,6 +7,11 @@ class BlenderStatusMonitor:
         self.max_count = max_count
         self.filename = filename
         
+        self.progress = {
+            '1': False,
+            '2': False
+        }
+        
         # initialize file
         open(self.filename, "w+").close() # This will clear file content
         
@@ -17,14 +22,24 @@ class BlenderStatusMonitor:
         self.progress_file = open(self.filename, "a")
      
         # ........ check status
+        #steps = [step1_result, step2_result]
+        
+        step1_result = self.checkStep1()
+        step2_result = self.checkStep2()
+        
+        print("step1 result is: " + step2_result)
         
         self.counter += 1
         if (self.counter >= self.max_count):
             self.appendToFile("tutorial completed")
             self.progress_file.close()
             return None
-        else:    
-            self.appendToFile("Timer Event " + str(self.counter))
+        else:
+            if (step1_result != ""): 
+                self.appendToFile(step1_result)
+            if (step2_result != ""): 
+                self.appendToFile(step2_result)
+                
             self.progress_file.close()
             return 5.0
          
@@ -34,6 +49,34 @@ class BlenderStatusMonitor:
     def appendToFile(self, str):
         self.progress_file.write("\n")
         self.progress_file.write(str)
-        
+    
+    # status checks
+    
+    #adding a cube check
+    def checkStep1(self):
+        if !self.progress['1']:
+            for item in bpy.data.objects:
+                if item.name == 'Cube':
+                    self.progress['1'] = True
+                    return "1_done"
+            return ""
+        else:
+            return ""
+    
+    #moving with G check
+    def checkStep2(self):
+        if self.progress['1'] and !self.progress['2']
+            if bpy.data.objects['Cube'].location.x != 0.0 or bpy.data.objects['Cube'].location.y != 0.0 or bpy.data.objects['Cube'].location.z != 0.0 :
+                self.progress['2'] = True
+                return "2_done"
+            return ""
+        else:
+            return ""
+   
+   def setStepTrue(self):
+       for step in self.progress:
+           step = True
+           return step + "_done"
+       
 if __name__ == "__main__":
     BlenderStatusMonitor(10, "/users/vivian/Documents/blender/progress.txt") 
