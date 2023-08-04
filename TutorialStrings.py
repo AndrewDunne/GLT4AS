4ba7e143c494038aa2bb942495079cfe5c6248fe
Vivian <82780605+vivianknee@users.noreply.github.com>
2023-08-03 23:27:45 -0700

    Create TutorialStrings.py

---
+++ TutorialStrings.py
@@ -0,0 +1,21 @@
+steps = {
+    1: ""
+}
+
+instructions = {
+    'i1': "Welcome to Blender! Blender is a 3D modeling software, which means you can use it to create 3D objects, simulate 3D scenes, make art, etc. In this tutorial, you’ll learn the basics of controlling the software and how to make your own simple objects!",
+    'i2':"To start, let’s explain what’s on screen. The main window is called the 3D viewport. This is where you see the 3D scene, and all of the objects inside of it. You start with a cube, light, and camera but there can be many types of objects",
+    'i3':"To move through the tutorial, press ‘Continue’. Sometimes, you will have to perform an action in order to move onto the next step",
+    4:"",
+    5:"",
+    6:"",
+    7:"",
+    8:"",
+    9:"",
+    10:"",
+    11:"",
+    12:"",
+    13:"",
+    14:"",
+    15:""
+}
