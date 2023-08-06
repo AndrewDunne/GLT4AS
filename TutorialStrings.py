12fc5d3643a4467612a793933aa2de7f66a57320
Vivian <82780605+vivianknee@users.noreply.github.com>
2023-08-05 21:50:48 -0700

    Update TutorialStrings.py

--- TutorialStrings.py
+++ TutorialStrings.py
@@ -4,18 +4,55 @@ steps = {
 
 instructions = {
     'i1': "Welcome to Blender! Blender is a 3D modeling software, which means you can use it to create 3D objects, simulate 3D scenes, make art, etc. In this tutorial, you’ll learn the basics of controlling the software and how to make your own simple objects!",
-    'i2':"To start, let’s explain what’s on screen. The main window is called the 3D viewport. This is where you see the 3D scene, and all of the objects inside of it. You start with a cube, light, and camera but there can be many types of objects",
+    'i2':"To start, let’s explain what’s on screen. The main window is called the 3D viewport. This is where you see the 3D scene, and all of the objects inside of it. You start with a cube, light, and camera but for this tutorial, the cube will not be present yet ",
     'i3':"To move through the tutorial, press ‘Continue’. Sometimes, you will have to perform an action in order to move onto the next step",
-    4:"",
-    5:"",
-    6:"",
-    7:"",
-    8:"",
-    9:"",
-    10:"",
-    11:"",
-    12:"",
-    13:"",
-    14:"",
-    15:""
+    'i4':"How do you navigate this 3D scene? This tutorial will assume you are using a computer mouse with a mouse wheel. To orbit the camera, hold down the middle mouse button and move the mouse around, or left click and drag the gizmo in the top right of the viewport.",
+    'i5':"To zoom the camera, scroll up or down on the mouse wheel, or left click and drag the magnifying glass icon in the top right.",
+    'i6':"To move the camera, hold down shift and the middle mouse button and move the mouse, or left click and drag the hand icon in the top right. These are the three basic ways to look around!" ,
+    'i7':"If you want to look into the scene from a specific direction, such as from the top or bottom, you can click the red, green, or blue circles in the 3D gizmo. X and -X show left and right views, Y and -Y show front and back views, and Z and -Z show top and bottom views",
+    'i8': "Next Up: Adding objects",
+    'i9':"Let’s learn how to add objects to the scene. There are a number of different types of objects in Blender. For now let’s focus on meshes, which are the most common kind of object.",
+    'i10':"To add a mesh into the scene, click ‘add’ button at the top of the 3D viewport. From here, find the object that you want to add - let’s add a cube, which is under the ‘Mesh’ category. You can also press Shift+A on the keyboard to bring up this menu quickly.",
+    #add cube check
+    'i11':"Good Job on adding your first object!",
+    'i12': "Moving on to Deleting Objects and Undo/Redo",
+    'i13':"There’s an object sitting in the scene that you don’t want any more - to delete it, select it, click 'x' on your keyboard, and press delete. Try removing the cube in the scene right now",
+    #delete cube check
+    'i14':"Cube deleted. Simple as that!",
+    'i15':"Just in case, if you’ve undone an action that you actually didn’t want to undo, you can ‘redo’ it by pressing Shift+Ctrl+Z. (CMD + Z on mac)",
+    'i16':"Moving onto new object settings!",
+    'i17':"Whenever you add a new object to the scene, Blender gives you some options that you can change. Add a circle to the scene and when the ‘add circle’ pop-up appears in the bottom left, click it to expand the options.",
+    'i18':"By lowering the amount of vertices, you can make a polygon like a triangle or square, increase the radius of the circle, and fill the circle in! Give the circle 6 vertices to make a hexagon, and set the fill type to ‘N-Gon’ to fill it in.",
+    #vertices on circle check
+    'i19':"You made a hexagon! Good Job!",
+    'i20':"The next lesson will teach you about the 3 most basic actions you can perform which are moving, rotating, and scaling.",
+    'i21':"Clear the scene and add a cube. Select it and press the G key. G stands for grab! Move the mouse around until you’re satisfied with where the cube is, and then left click or press the enter key to place it down. You can move on a specific axis by pressing G and then x, y, or z. Try it!",
+    #move cube check
+    'i22':"Good job moving the cube!",
+    'i23':"To rotate the cube, press R while selecting it. The same rules apply for rotating; move the mouse cursor to spin the cube, and then left click/enter to confirm the rotation.",
+    #rotate cube check
+    'i24':"Good job rotating the cube!",
+    'i25':"Finally, to scale the cube, press S.",
+    #scale cube check
+    'i26':"Good job on scaling! You just learned the 3 basic actions for objects!",
+    'i27': "Sometimes while performing an action, such as moving an object, you decide that you don’t want to do it. In order to cancel your current action, press the ‘Esc’ key! Try it out",
+    'i28':"Now lets learn about Selecting Objects",
+    #ask user to add cone
+    'i29':"Clear the scene and add a cone (under meshes). Left click to select it. You’ll see it become highlighted in orange when selected, and left clicking anywhere else in the viewport will deselect it and remove the highlight",
+    #ask user to add 2 cones to scene
+    'i30':"Add 2 more cones! You can select multiple objects at once by left clicking and dragging in the viewport to box-select, or you can select one object and then shift-left click the other objects to add them to your selection. Try selecting all 3 cones in the scene.",
+    'i31':"When you have multiple objects selected, you can perform actions on all of them at once! Try deleting all the cones at the same time!",
+    'i32': "Nice! Now lets move on to flat and smooth shading",
+    'i33': "Add a UV-Sphere. You might have noticed that it looks like a golf ball - they have many small, square faces. If you want the sphere to appear smooth, select the sphere and right click to bring up a menu. Then, select the top option: “Shade Smooth”",
+    #shade smooth check
+    'i34': "Good Job Now it looks more like a bouncy ball!",
+    'i35': "Smooth shading looks nice on objects that should be smooth, but on angular things like cubes it doesn’t look right - the default ‘flat’ shading looks better. However, some objects like cylinders have some flat parts and some smooth parts. To account for this, there is another option - auto smooth shading.",
+    'i36': "Remove the sphere and add a cylinder. Select and right click it, and then select the “Shade Auto Smooth” option.",
+    #check shade auto smooth
+    'i37': "Good job! Now you know how to change the shading of meshes!",
+    'i38': "Now lets move on to duplicating objects!",
+    'i39': "While in most programs you use the hotkeys Ctrl+C and Ctrl+V in order to duplicate things, in Blender there is a single hotkey instead: Shift+D. This duplicates any selected objects!",
+    #check duplicate object
+    'i40': "Try using Shift+D to duplicate the chair in the scene, and then place it down wherever you like.",
+    'i41':"Nice!"
 }
